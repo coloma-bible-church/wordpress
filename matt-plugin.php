92150d2e2cb0372d272c0203c940c13d6599c488
Matthew Thomas <me@matthewathomas.com>
2024-02-05 16:48:41 -0500

    Update matt-plugin.php

--- matt-plugin.php
+++ matt-plugin.php
@@ -17,13 +17,62 @@ if ( ! function_exists( 'do_shortcode' ) ) {
     exit;
 }
 
-function longcode_shortcode( $atts , $content = null ) {
-	while ( $content ) {
-	    $after = do_shortcode( $content );
-	    if ( $after === $content ) {
-	        break;
-	    }
-	}
-	return $content;
+function longcode_shortcode( $atts , string $content = null ) {
+    $xml = new DOMDocument();
+    @ $xml->loadXML( $content );
+    foreach ( $xml->childNodes as $child ) {
+        foreach ( $child->childNodes as $grandchild ) {
+            if ( ! is_a( $grandchild, 'DOMText' ) ) {
+                continue;
+            }
+            /**
+             * @var DOMText $grandchild
+             */
+            $json = $grandchild->wholeText;
+            $array = json_decode( $json, true );
+            return longcode_shortcode_core( $array );
+        }
+    }
+    return $content;
 }
+
+/**
+ * @param array|mixed $array
+ */
+function longcode_shortcode_core( $array ) {
+    if ( is_array( $array ) ) {
+        if ( array_key_exists( 'name', $array ) ) {
+            // Treat as shortcode definition
+            $name = $array['name'];
+            $shortcode = '[' . $name;
+            if ( array_key_exists( 'atts', $array ) ) {
+                $atts = $array['atts'];
+                foreach ( $atts as $key => $value ) {
+                    $value = longcode_shortcode_core( $value );
+                    $shortcode .= ' ' . $key . '="' . $value . '"';
+                }
+            }
+            if ( array_key_exists( 'content', $array ) ) {
+                $content = $array['content'];
+                $content = longcode_shortcode_core( $content );
+                $shortcode .= ']' . $content . '[/' . $name;
+            }
+            $shortcode .= ']';
+
+            return do_shortcode( $shortcode );
+        } else {
+            // Concat elements
+            return array_reduce(
+                $array,
+                function( $carry, $item ) {
+                    return $carry . longcode_shortcode_core( $item );
+                },
+                ''
+            );
+        }
+    } else {
+        return $array;
+    }
+}
+
 add_shortcode( 'longcode', 'longcode_shortcode' );
